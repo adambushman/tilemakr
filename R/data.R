692ab42aa1681639f774d440b46f4797a24f2127
Adam Bushman <adam.bushman1@gmail.com>
2023-07-06 20:17:55 -0600

    Adjusted demo data

--- R/data.R
+++ R/data.R
@@ -1,38 +1,16 @@
-#' Tabular layout for tile map
+#' Demo Date
 #'
-#' A grid style layout for creating a tile map. Each column and row describes
-#' elements to great the map. This data type should be converted to a matrix
-#' using `tabular_to_matrix()`.
+#' A collection of data structures for experimenting with {tilemakr} functions.
 #'
-#' @format ## `tabular_layout`
-#' A data frame with 4 rows and 4 columns:
-#' \describe{
-#'   \item{V1}{Layout elements}
-#'   \item{V2}{Layout elements}
-#'   \item{V3}{Layout elements}
-#'   \item{V4}{Layout elements}
-#' }
-"tabular_layout"
+#' @format ## `demo_data`
+#' A list of matrices and data frames
+"demo_data"
 
-#' Tabular layout for tile map
-#'
-#' A data set intended to illustrate how to join generated tile map data to
-#' a corresponding data set with variables of interest.
-#'
-#' @format ## `sales_data`
-#' A data frame with 7 rows and 2 columns:
-#' \describe{
-#'   \item{id}{Unique identifiers for the tile shapes}
-#'   \item{sales}{Sales figures for each tile shape}
-#' }
-"sales_data"
-
-#' List of matrices to create tile maps
+#' Tile Layouts
 #'
 #' Pre-generated layout matrices for quick tile making, then join to and plot
 #' with your own data sets.
 #'
 #' @format ## `tile_layouts`
-#' A list of matrices for creating tile maps using any shape supported
-#' by the {tilemaker} functions.
+#' A list of matrices
 "tile_layouts"
--- R/make_tiles.R
+++ R/make_tiles.R
@@ -129,3 +129,49 @@ plot_tiles <- function(df, labels = TRUE) {
   }
   return(main)
 }
+
+
+
+#' Preview Shapes
+#'
+#' `preview_shapes()` shows a preview of what tile maps would look like with
+#' different shapes.
+#'
+#' @returns A {ggplot2} object
+#' @import ggplot2
+#' @examples
+#' preview_shapes()
+#' @export
+plot_tiles <- function(df, labels = TRUE) {
+  missing_cols = setdiff(c("x", "y", "center_x", "center_y", "id"), names(df))
+  if(length(missing_cols) > 0) {
+    stop(paste("This data frame is missing the required columns:", paste(missing_cols, collapse = ", ")))
+  }
+  if(is.null(labels)) {
+    labels = TRUE
+  }
+  if(!is.logical(labels)) {
+    stop("Please indicate if tile labels should be included or not with TRUE/FALSE")
+  }
+
+  main <-
+    ggplot2::ggplot() +
+    ggplot2::geom_polygon(
+      ggplot2::aes(x, y, group = id),
+      df
+    )
+
+  if(labels) {
+    main <-
+      main +
+      ggplot2::geom_text(
+        ggplot2::aes(
+          center_x, center_y,
+          label = id
+        ),
+        color = "white",
+        unique.data.frame(df[,c("center_x", "center_y", "id")])
+      )
+  }
+  return(main)
+}
